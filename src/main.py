12308d21faff34bce804c5d6d793913e09dfbc44
blakehulett7 <blake.hulett7@gmail.com>
2024-06-06 17:45:51 -0500

    NUKE IT, back to pygame baby i can feel the palpable indecision!!!

--- src/main.py
+++ src/main.py
@@ -1,17 +1,1 @@
-from objects import Window
-from logic import Game_Master
-
-
-def main():
-    screen_x = 900
-    screen_y = 600
-    win = Window(screen_x, screen_y)
-
-    gm = Game_Master(screen_x=screen_x, screen_y=screen_y, canvas=win.canvas)
-    gm.generate_balls(5, "red")
-    gm.draw_balls()
-
-    win.wait_for_close()
-
-
-main()
+import pygame
